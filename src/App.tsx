d397b7514cd6f951f2fd69ebcf568861a9569641
Liam Hemo <liamhemo64@gmail.com>
2025-12-30 14:19:26 +0200

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import MainPage from "./components/MainPage";
 import AtlasAppBar from "./components/appBar/AtlasAppBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <AtlasAppBar />
